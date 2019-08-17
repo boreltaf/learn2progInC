16c167858cc3a9dffb284671deef24930dccb6db
Coursera Learner <nobody@nowhere.nul>
2019-08-17 04:16:54 +0000

    did assignment

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -6,6 +6,7 @@ size_t maxSeq(int * array, size_t n);
 int main(){
   int array1[]={1,1,1,1,1,1,11};
   int array2[]={1,2,5,3,2,1,6,7,8};
+  int array3[1]={};
   int array4[]={9,8,7,6,5,4,3,2,1};
   int array5[]={100,16,166,16,1666};
   int array6[]={-5,-6,-7,-3,-2,7};
@@ -17,6 +18,8 @@ int main(){
   if( maxSeq(array1, 7)!=2){
     return EXIT_FAILURE;}
   else if( maxSeq(array2,9) !=4){
+    return EXIT_FAILURE;}
+  else if( maxSeq(array3,0) !=0){
     return EXIT_FAILURE;}
   else if( maxSeq(array4,9) !=1){
     return EXIT_FAILURE;}
