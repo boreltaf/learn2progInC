436650790d00a97454b0f058d10d79f86464dc29
Coursera Learner <nobody@nowhere.nul>
2019-09-23 17:31:46 +0000

    did assignment 32

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -3,20 +3,96 @@
 #include <string.h>
 #include "kv.h"
 
-
+kvpair_t *splitpair(char *line, size_t size){
+  if(line==NULL){
+    fprintf(stderr, "line is NULL\n");
+    return NULL;
+  }
+  if(size == 0){
+    fprintf(stderr, "line does not containt any character\n");
+    return NULL;
+  }
+  kvpair_t *pair =malloc(sizeof(*pair));
+  pair->key = NULL;
+  pair->value =NULL;
+  int i=0;
+  while((line[i]!='=')&&(i<size)){
+      pair->key = realloc(pair->key, (i+1)*sizeof(*(pair->key)));
+      pair->key[i] = line[i];
+      i++;
+  };
+  if(i==0){
+     free(pair);
+     fprintf(stdout, "there is not key\n");
+     return NULL;
+    }
+  if(i >= size-1){
+    free(pair->key);
+    free(pair);
+    fprintf(stdout, "there is not value\n");
+    return NULL;
+    }
+  pair->key = realloc(pair->key, (i+1)*sizeof(*(pair->key)));
+  pair->key[i]='\0';
+  int j=i;
+  for(j=i; j<size; j++){
+    pair->value = realloc(pair->value, (j-i+1)*sizeof(*(pair->value)));
+    pair->value[j-i]=line[j];
+  }
+  pair->value = realloc(pair->value, (j-i+1)*sizeof(*(pair->value)));
+  pair->value[j-i] = '\0';
+  return pair;
+}
 
 kvarray_t * readKVs(const char * fname) {
-  //WRITE ME
+  FILE *f = fopen(fname, "r");
+  kvarray_t *kvarray=malloc(sizeof(*kvarray));
+  kvarray->arraykv = NULL;
+  char *line=NULL;
+  size_t x=0;
+  ssize_t lsize=0;
+  int i=0;
+  while((lsize=getline(&line, &x,f))>0){
+    kvarray->arraykv = realloc(kvarray->arraykv, (i+1)*sizeof(*(kvarray->arraykv)));
+    size_t size = lsize;
+    kvarray->arraykv[i] = splitpair(line, size-1);
+    i++;
+    line = NULL;
+  };
+  kvarray->kvlenght = (i>0)? i: i;
+  free(line);
+  if(kvarray->arraykv == NULL){
+    fprintf(stdout, "the file was empty\n");
+    return NULL;
+  }
+  if(fclose(f)!=0){
+    fprintf(stderr, "the file couldn't close correctly\n");
+    return NULL;
+  }
+  return kvarray;
 }
 
 void freeKVs(kvarray_t * pairs) {
-  //WRITE ME
+  for(int i=0; i<pairs->kvlenght; i++){
+    free(pairs->arraykv[i]->key);
+    free(pairs->arraykv[i]->value);
+    free(pairs->arraykv[i]);
+  }
+  free(pairs->arraykv);
+  free(pairs);
 }
 
 void printKVs(kvarray_t * pairs) {
-  //WRITE ME
+  for(int i=0; i < pairs->kvlenght; i++){
+    fprintf(stdout,"key = '%s' value = '%s'\n", pairs->arraykv[i]->key, pairs->arraykv[i]->value);
+  }
 }
 
 char * lookupValue(kvarray_t * pairs, const char * key) {
-  //WRITE ME
+  for(int i=0; i < pairs->kvlenght; i++){
+    if(strcmp(pairs->arraykv[i]->key, key)==0){
+      return pairs->arraykv[i]->key;
+    }
+  }
+  return NULL;
 }
