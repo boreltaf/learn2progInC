e3ada63bda1bf6be60e217c68fc7bd74e913c983
Coursera Learner <nobody@nowhere.nul>
2019-10-03 17:47:33 +0000

    did assignment

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -5,6 +5,7 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
   if(index < fc->n_decks){
      fc->decks[index].cards=realloc(fc->decks[index].cards, ++(fc->decks[index].n_cards)*sizeof(*(fc->decks[index].cards)));
      fc->decks[index].cards[fc->decks[index].n_cards -1] = ptr;
+     
      }
   else{
     for(int i=fc->n_decks; i<index; i++){
@@ -25,12 +26,14 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
     perror(" ERROR! there is no cards in the future deck \n");
     return;
   }
+  int index = 0;
   for(int i=0; i< fc->n_decks; i++){
     if(fc->decks[i].cards!= NULL){
       for(int j=0; j< (fc->decks[i]).n_cards; j++){
-	  (fc->decks[i]).cards[j]->value = deck->cards[i]->value;
-	  (fc->decks[i]).cards[j]->suit = deck->cards[i]->suit;
+	  (fc->decks[i]).cards[j]->value = deck->cards[index]->value;
+	  (fc->decks[i]).cards[j]->suit = deck->cards[index]->suit;
 	}
+      index++;
     }
   }
 }
